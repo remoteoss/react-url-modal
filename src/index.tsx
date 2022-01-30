81fb754133d91dc21cabaf556b40c59f0c71b81d
Sara Vieira <hey@iamsaravieira.com>
2022-01-30 03:08:32 +0100

    clean package.json

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,3 @@
-export { URLModal, openModal, closeModal } from './UrlModal';
+export { URLModal, openModal, closeModal } from './URLModal';
 export { encodeUrlParams, decodedUrlParams, isModalOpen } from './helpers';
 export { Modal } from './StyledModal';
