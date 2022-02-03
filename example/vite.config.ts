6e9309e2bee3defce80e55aa43050003ceb0acfb
Francisco Sousa <francisco@remote.com>
2022-02-03 17:48:05 +0000

    Remove console lul

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-console.log(__dirname);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
