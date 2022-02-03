767640de5e3bc127229e4e2b6d1f943e6448e667
Francisco Sousa <francisco@remote.com>
2022-02-03 17:47:41 +0000

    Make the example paths prettier

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+console.log(__dirname);
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: /^react-url-modal$/,
+        replacement: __dirname + '/../dist/react-url-modal.esm.js',
+      },
+      {
+        find: /^react-url-modal\/Modal$/,
+        replacement: __dirname + '/../dist/Modal/index.js',
+      },
+    ],
+  },
   server: {
     fs: {
       // Allow serving files from one level up to the project root
