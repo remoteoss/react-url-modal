3a993b2c7e377351a449dd63a404fcd1dde579d2
Sara Vieira <sara.vieira@remote.com>
2022-02-09 11:35:47 +0100

    pass modal name to all wrappers

--- src/URLModal.tsx
+++ src/URLModal.tsx
@@ -117,6 +117,7 @@ export const URLModal = ({
               window.dispatchEvent(new Event(`${modalName}-submit`))
             }
             params={getData()}
+            modalName={modalName}
             {...extraProps}
           />
         </Suspense>
