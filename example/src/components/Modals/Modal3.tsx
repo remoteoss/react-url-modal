ceb0dacb7af3afa046b4eaa06167afdaf9815a0d
Francisco Sousa <francisco.sousa@hey.com>
2023-09-23 17:02:27 +0100

    Fix build and types

--- example/src/components/Modals/Modal3.tsx
+++ example/src/components/Modals/Modal3.tsx
@@ -1,5 +1,5 @@
 import { Modal } from 'react-url-modal/Modal';
 
-export default function Modal3(props: unknown) {
+export default function Modal3(props: any) {
   return <Modal {...props}>I am loaded from another file</Modal>;
 }
